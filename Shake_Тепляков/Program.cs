52c4c0d27bf33db1ac7fa27e7629d0191d399625
kirillColeBou <kirillteplycov888@gmail.com>
2024-11-21 21:21:30 +0500

    Созданный метод Send. Тепляков

--- Shake_Тепляков/Program.cs
+++ Shake_Тепляков/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Common;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +14,37 @@ namespace Shake_Тепляков
     internal class Program
     {
         public static List<Leaders> Leaders = new List<Leaders>();
-        public static List<ViewModelUserSettings> ViewModelUserSettings = new List<ViewModelUserSettings>();
-        public static List<ViewModelGames> ViewModelGames = new List<ViewModelGames>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5001;
         public static int MaxSpeed = 15;
+
+        private static void Send()
+        {
+            foreach(var User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex) 
+                { 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n " + ex.Message);   
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
