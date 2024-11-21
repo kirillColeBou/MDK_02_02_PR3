e86af094fc0389550ebbaf1abd0f25357781cee2
kirillColeBou <kirillteplycov888@gmail.com>
2024-11-21 20:28:14 +0500

    Добавлены системные файлы

--- Shake_Тепляков/Program.cs
+++ Shake_Тепляков/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shake_Тепляков
 {
-    internal class Program
+    public class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> ViewModelUserSettings = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> ViewModelGames = new List<ViewModelGames>();
+
         static void Main(string[] args)
         {
         }
