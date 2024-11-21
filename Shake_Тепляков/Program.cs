285fb0591ac262f7fb1ee3f8f06f501393d6cf6d
kirillColeBou <kirillteplycov888@gmail.com>
2024-11-21 21:08:39 +0500

    Добавленные переменные сервера. Тепляков

--- Shake_Тепляков/Program.cs
+++ Shake_Тепляков/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
@@ -9,6 +10,12 @@ namespace Shake_Тепляков
 {
     internal class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> ViewModelUserSettings = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> ViewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
+
         static void Main(string[] args)
         {
         }
